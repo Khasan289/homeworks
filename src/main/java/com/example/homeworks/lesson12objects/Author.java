063ba2f02da6572385a26d2e48847274739e2939
Khasan289 <sarmatovalan289@gmail.com>
2023-01-16 23:04:28 +0300

    ДЗ по уроку 1.13. Методы объектов.

--- src/main/java/com/example/homeworks/lesson12objects/Author.java
+++ src/main/java/com/example/homeworks/lesson12objects/Author.java
@@ -1,5 +1,7 @@
 package com.example.homeworks.lesson12objects;
 
+import java.util.Objects;
+
 public class Author {
     private String name;
     private String surname;
@@ -23,6 +25,16 @@ public class Author {
         return name + " " + surname;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return name.equals(author.name) && surname.equals(author.surname);
+    }
 
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
 }
--- src/main/java/com/example/homeworks/lesson12objects/Main.java
+++ src/main/java/com/example/homeworks/lesson12objects/Main.java
@@ -1,10 +1,6 @@
 package com.example.homeworks.lesson12objects;
 
 public class Main {
-    public static void printBook(String nameBook, Author author, int year) {
-        System.out.println(nameBook + " " + author + " " + year);
-    }
-
 
 
     public static void main(String[] args) {
@@ -13,11 +9,9 @@ public class Main {
         Book threePiglets = new Book("Три поросенка.", sergeiMikhalkov, 2011);
         Author sunTzu = new Author("Сунь","Цзы");
         Book artOfWar = new Book("Искусство войны.", sunTzu, 2023);
-        artOfWar.setPublicationYear(1992);
+        artOfWar.setPublicationYear(2003);
         artOfWar.printBook();
 
-        //printBook(artOfWar.getNameBook(), artOfWar.getAuthor(), artOfWar.getPublicationYear());
-        //printBook(threePiglets.getNameBook(), threePiglets.getAuthor(), threePiglets.getPublicationYear());
 
 
 
